291790428a2958c63865f3a4ff27726a90ee4478
lilynoel <lilydnas@gmail.com>
2022-07-11 23:21:50 +1000

    add location enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,16 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+
+  enum location: {
+    NSW: 1,
+    QLD: 2,
+    TAS: 3,
+    VIC: 4,
+    WA: 5,
+    SA: 6,
+    NT: 7,
+    ACT: 8
+  }
 end
