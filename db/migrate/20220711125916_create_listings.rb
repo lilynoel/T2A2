848f0ea99fded7d151c1b8c401a3715fb0ac6150
lilynoel <lilydnas@gmail.com>
2022-07-11 23:03:39 +1000

    available boolean default

--- db/migrate/20220711125916_create_listings.rb
+++ db/migrate/20220711125916_create_listings.rb
@@ -5,7 +5,7 @@ class CreateListings < ActiveRecord::Migration[6.1]
       t.string :breed
       t.string :location
       t.integer :fee
-      t.boolean :available
+      t.boolean :available, default: true
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
