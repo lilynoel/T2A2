0ac2f01130deb066b91b752b1d7b9c452dd923dd
lilynoel <lilydnas@gmail.com>
2022-07-23 19:42:01 +1000

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :listings
 
-  has_many :sold_olders, foreign_key: "seller_id", class_name: "Order"
+  has_many :sold_orders, foreign_key: "seller_id", class_name: "Order"
   has_many :bought_orders, foreign_key: "buyer_id", class_name: "Order"
   
 end
